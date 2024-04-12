8616ad87767c2ffe1b35fd37093ccc5e300d1bea
Owen <carneyof@outlook.com>
2024-04-11 23:47:09 -0400

    Added list view to accountform.

--- PasswordManager-Desktop/AccountForm.Designer.cs
+++ PasswordManager-Desktop/AccountForm.Designer.cs
@@ -28,18 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            listView1 = new ListView();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(540, 33);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(248, 405);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.Location = new Point(624, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Accounts";
             // 
             // AccountForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(listView1);
             Name = "AccountForm";
             Text = "Account Form";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListView listView1;
+        private Label label1;
     }
 }
